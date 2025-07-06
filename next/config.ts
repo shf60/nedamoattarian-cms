aba4a01446c34fa476d713957324e05c714a22cf
Sadjad Haghani <47915954+shf60@users.noreply.github.com>
2025-07-06 20:02:57 +0330

    Update config.ts

--- next/config.ts
+++ next/config.ts
@@ -1,5 +1,5 @@
-export const defaultLocale = "en" as const;
-export const locales = ["en", "fr"] as const;
+export const defaultLocale = "fa" as const;
+export const locales = ["en", "fa"] as const;
 
 export type Locale = (typeof locales)[number];
 
