e27366f75a65321a5e9ef6b678d04415cd6423ae
sajjadHaghani <sajjad.haghani.it@gmail.com>
2025-07-06 20:25:09 +0330

    change lang order

--- next/config.ts
+++ next/config.ts
@@ -1,5 +1,5 @@
 export const defaultLocale = "fa" as const;
-export const locales = ["en", "fa"] as const;
+export const locales = ["fa", "en"] as const;
 
 export type Locale = (typeof locales)[number];
 
--- next/i18n.config.ts
+++ next/i18n.config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'fa',
-  locales: ['en', 'fa']
+  locales: ['fa', 'en']
 } as const
 
 export type Locale = (typeof i18n)['locales'][number]
