312e4d71a283ba0dcd41a23aea1f36bfb8062bce
sajjadHaghani <sajjad.haghani.it@gmail.com>
2025-08-17 23:42:00 +0330

    fix couldinary plugin

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,11 +1,15 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-cloudinary',
+      provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
